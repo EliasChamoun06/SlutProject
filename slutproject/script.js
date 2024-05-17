007d04885e750e6b1b549feb23060f8a218dee45
Elias <elias.chamoun@skola.taby.se>
2024-05-17 23:08:45 +0200

    sista commit

--- slutproject/script.js
+++ slutproject/script.js
@@ -1,3 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleSwitch = document.getElementById("toggleSwitch");
+  const body = document.getElementById("menu-tacos");
+
+  console.log(body);
+
+  body.classList.add("light-mode");
+
+  console.log(toggleSwitch);
+
+  toggleSwitch.addEventListener("change", () => {
+    if (toggleSwitch.checked) {
+      body.classList.replace("light-mode", "dark-mode");
+    } else {
+      body.classList.replace("dark-mode", "light-mode");
+    }
+  });
+  console.log("hej");
+});
 // Dropdown button - Kontaka oss
 document.addEventListener("DOMContentLoaded", function () {
   var dropdownButton = document.querySelector(".dropdown-button");
@@ -15,3 +34,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZWxpYXMwNiIsImEiOiJjbHdiMjdqMGkwaG5xMmpwYXZjZWdrMWd0In0.SGLTbgoucFssC4BF20ZP6Q";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [18.0686, 59.3293],
+  zoom: 15,
+});
+
+const marker = new mapboxgl.Marker().setLngLat([18.0686, 59.3293]).addTo(map);
+
+const popup = new mapboxgl.Popup({ offset: 25 })
+  .setLngLat([18.0686, 59.3293])
+  .setHTML("<h3>Stockholm city</h3><p>Står precis i Gustav adolfs torg</p>")
+  .addTo(map);
